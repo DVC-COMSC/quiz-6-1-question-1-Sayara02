a802d296ab249f58103af89746b65ad559044ef2
Sayara02 <azizovna02@gmail.com>
2024-03-15 21:34:24 -0700

    errors

--- main.hpp
+++ main.hpp
@@ -10,13 +10,34 @@ bool isPrime(int num) {
         return true;
     if (num % 2 == 0)
         return false;
-    for (imt i = 3; i <= sqrt(num); i += 2){
+    for (int i = 3; i <= sqrt(num); i += 2){
         if (num % i == 0)
             return false;
     }
     return true;
 
 }
+void getTwoValues(int &begin, int &end) {
+    do {
+        std::cout << "Enter two imteger values (begin < end): ";
+        std::cin >> begin >> end;
+    }while (begin >= end);
+}
+int getNextPrime(int begin) {
+    int next = begin + 1;
+    while (!isPrime(next)) {
+        next++;
+    }
+    return next;
+}
+int getPrevPrime(int end) {
+    int prev = end - 1;
+    while (!isPrime(prev)) {
+        prev--;
+    }
+    return prev;
+}
+#endif
 // Declare Function Prototypes
 //********************
 
