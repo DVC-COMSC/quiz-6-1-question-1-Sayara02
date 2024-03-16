b46fd7644f6beb2127b978ea861cfe1f8f9f0019
Sayara02 <azizovna02@gmail.com>
2024-03-15 21:17:47 -0700

    add code 2

--- main.cpp
+++ main.cpp
@@ -25,6 +25,12 @@ int getPrevPrime(int end) {
 	}
 	return prev;
 }
+void getTwoValues(int &begin, int &end) {
+	do {
+	cout << "Enter two integer values (begin < end): ";
+	cin >> begin >> end:
+} while (begin >= end);
+}
 int main()
 {
 	int begin, end;
